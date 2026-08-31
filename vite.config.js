9d305e532a164315ac762643afb187ab3cc2b98e
benben-prog <benben@benb3n.com>
2026-08-31 07:38:02 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss({
-      modernColors: false,
-    }),
+    tailwindcss(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
